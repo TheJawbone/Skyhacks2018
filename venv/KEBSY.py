b8637ea49a72d9b13c5426cf2747072da154c5ab
Ben Ghazi <michal.t.kret@gmail.com>
2018-11-17 19:31:55 +0100

    czytanie kodow

--- venv/KEBSY.py
+++ venv/KEBSY.py
@@ -40,10 +40,13 @@ pts31 = np.float32([[width/12, height/3],[width/2, height/3],[width/12, 2*height
 pts32 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 pts41 = np.float32([[width/2 - width/12, height/3],[width - (width/12), height/3],[width/2 - width/12, 2*height/3],[width - width/12,height - 2*height/3]])
 pts42 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+pts51 = np.float32([[width/3 - width/12, height/3],[2*width/3 + (width/12), height/3],[width/3 - width/12, height - height/12],[2*width/3 + width/12,height - height/12]])
+pts52 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 M1 = cv2.getPerspectiveTransform(pts11,pts12)
 M2 = cv2.getPerspectiveTransform(pts21,pts22)
 M3 = cv2.getPerspectiveTransform(pts31,pts32)
 M4 = cv2.getPerspectiveTransform(pts41,pts42)
+M5 = cv2.getPerspectiveTransform(pts51,pts52)
 
 imagelist = []
 
@@ -51,6 +54,7 @@ imagelist.append(cv2.warpPerspective(image,M1,imgsize))
 imagelist.append(cv2.warpPerspective(image,M2,imgsize))
 imagelist.append(cv2.warpPerspective(image,M3,imgsize))
 imagelist.append(cv2.warpPerspective(image,M4,imgsize))
+imagelist.append(cv2.warpPerspective(image,M5,imgsize))
 
 
 index=1
@@ -71,7 +75,43 @@ for image in imagelist:
     filename = "{}.png".format(os.getpid())
     cv2.imwrite(filename, image)
 
+    firstlinecount=2
+    secondlinecount=2
+    thirdlinecount=8
+    firstnumber = []
+    secondnumber=[]
+    thirdnumber=[]
+    listindex=0
     text = pytesseract.image_to_string(Image.open(filename))
+    linelist = str.splitlines(text)
+    if len(linelist) > 2:
+        while firstlinecount != 0 and listindex < len(linelist):
+            firstlinecount=2
+            firstnumber = []
+            for character in linelist[listindex]:
+                if str.isdecimal(character) and firstlinecount > 0:
+                    firstnumber.append(character)
+                    firstlinecount -= 1
+            listindex += 1
+        while secondlinecount != 0 and listindex < len(linelist):
+            secondlinecount=2
+            secondnumber = []
+            for character in linelist[listindex]:
+                if str.isdecimal(character) and secondlinecount > 0:
+                    secondnumber.append(character)
+                    secondlinecount -= 1
+            listindex += 1
+        while thirdlinecount != 0 and listindex < len(linelist):
+            thirdlinecount=8
+            thirdnumber = []
+            for character in linelist[listindex]:
+                if str.isdecimal(character) and thirdlinecount > 0:
+                    thirdnumber.append(character)
+                    thirdlinecount -= 1
+            listindex +=1
+        print(firstnumber)
+        print(secondnumber)
+        print(thirdnumber)
     os.remove(filename)
     print("Result: ")
     print(index)
